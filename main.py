00951d1a3e1fbd970ebe833fe39abcd89ba5c0b9
Ravi Gordon <84871171+rg460@users.noreply.github.com>
2022-07-20 11:37:09 +0100

    Asci Art and Pseudo code created
    Aci Art was got from https://ascii.co.uk/art

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+# Flow chart -https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+# Plan
+# Ask the user if they want to go left or right and store in a varaible
+# if the user selects right then the user falls in a hole Game Over if the user selects left then move onto the next stage of game 
+# Ask user if they want to swim or wait and store in variable
+# if answer is swim then user is attacked by trout Game Over and if wait move onto next stage of the game
+# Ask the user to choose a door and store in a varaible 
+# if user chooses Blue Eaten by beasts Game Over . if the user chooses red Burned by fire Game Over . If user chooses Yellow then they win . Any Other answer would result in game over  
 
