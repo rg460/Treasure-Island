3fa1d167e8cf1098fce2e1ba65d1de07a7a3652a
Ravi Gordon <84871171+rg460@users.noreply.github.com>
2022-07-21 14:51:32 +0100

    final bit of game logic added

--- main.py
+++ main.py
@@ -26,26 +26,33 @@ print("Your mission is to find the treasure.")
 # Flow chart -https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
 #Write your code below this line 👇
+# if user chooses Blue Eaten by beasts Game Over . if the user chooses red Burned by fire Game Over . If user chooses Yellow then they win
 # Plan
-# Ask the user if they want to go left or right and store in a varaible
-  # Ensure users answer is not case sensitive
-# if the user selects right then the user falls in a hole Game Over if the user selects left then move onto the next stage of game 
+# Ask the user if they want to go left or right and store in a varaible [done]
+  # Ensure users answer is not case sensitive and create a variable[done]
+# if the user selects right then the user falls in a hole Game Over or any other input if the user selects left then move onto the next stage of game 
 # Ask user if they want to swim or wait and store in variable
   # Ensure users answer is not case sensitive
 # if answer is swim then user is attacked by trout Game Over and if wait move onto next stage of the game
 # Ask the user to choose a door and store in a varaible 
-  # Ensure users answer is not case sensitive
-# if user chooses Blue Eaten by beasts Game Over . if the user chooses red Burned by fire Game Over . If user chooses Yellow then they win . Any Other answer would result in game over  
+  # Ensure users answer is not case sensitiveo swim across:\n")
+   direction_answer_lower_case = direction_answer.lower(); . Any Other answer would result in game over  
 direction = input("You're at a crossroad. Where do you want to go? Type left or Type right:\n")
 direction_lower_case = direction.lower()
 if direction_lower_case == "right":
   print("You Fall into a hole . Game Over!\n")
 elif direction_lower_case == "left":
-   input("You've come to a lake. There is an island in the middle of the lake. Type wait to wait for a boat. Type swim to swim across:\n")
- direction_answer_lower_case = direction_answer.lower();
+   direction_answer = input("You've come to a lake. There is an island in the middle of the lake. Type wait to wait for a boat. Type swim t
 if direction_answer_lower_case == 'swim':
   print("Attacked by a mysterious beast that inhabits the lake")
 elif direction_answer_lower_case == 'wait':
-  boat_answer = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose")
+    boat_answer = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose:\n")      
+    boat_answer_lower_case = boat_answer.lower()
+if boat_answer_lower_case == 'blue':
+  print("You are Eaten by beasts. Game Over ")
+elif boat_answer_lower_case == 'red':
+  print("You are burned by fire. Game Over ")
+elif boat_answer_lower_case == 'yellow':
+  print("Congratualtions you chose the correct door  the treasure is in this room, you win!!") 
 else:
-  print("Invalid Direction Game over")
+  print("Invalid Answer Game over")
